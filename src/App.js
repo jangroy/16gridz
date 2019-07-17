779e27cb0032d575a40276b33e09c181d1953cb5
Roy Jang <jangroy@hotmail.com>
2019-07-17 01:18:06 -0700

    try out firebase storage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,29 @@ import Top from "./components/Top";
 import styled from "styled-components";
 import theme from "styled-theming";
 import PadsContext from "./context.js";
+import { firebaseConfig } from "./config/firebaseconfig";
+import * as firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+const storageRef = storage.ref("/samples/kicks/");
+
+storageRef
+  .listAll()
+  .then(result => {
+    console.log("results", result.items);
+    result.items.forEach(itemRef => {
+      itemRef.getDownloadURL().then(url => {
+        console.log("url", url);
+      });
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 const MainContainer = styled.div`
   box-sizing: border-box;
---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCiLAWdnLmweHg_XemaxTmcgBaWhVC_J-c",
+  authDomain: "gridz-3e12d.firebaseapp.com",
+  databaseURL: "https://gridz-3e12d.firebaseio.com",
+  projectId: "gridz-3e12d",
+  storageBucket: "gridz-3e12d.appspot.com",
+  messagingSenderId: "889370390015",
+  appId: "1:889370390015:web:d5258f39e8899bce"
+};
